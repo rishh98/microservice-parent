d7225e066dbb5f90bc8d9c2e5bffe639dca47a2c
Rishi <rishikesh.jswr@gmail.com>
2023-05-30 17:49:25 +0530

    RISHI-2023.5

--- product-service/src/main/java/com/microservice/rishi/product_service/service/ProductService.java
+++ product-service/src/main/java/com/microservice/rishi/product_service/service/ProductService.java
@@ -3,6 +3,10 @@ package com.microservice.rishi.product_service.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 import com.microservice.rishi.product_service.dto.ProductRequest;
 import com.microservice.rishi.product_service.dto.ProductResponse;
@@ -12,15 +16,18 @@ import com.microservice.rishi.product_service.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 
 
+
+
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class ProductService {
 	
 	private final ProductRepository productRepository;
 	
-	public ProductService(ProductRepository productRepository) {
-		this.productRepository=productRepository;
-	}
+//	public ProductService(ProductRepository productRepository) {
+//		this.productRepository=productRepository;
+//	}
 	
 	
 	public void createProduct(ProductRequest productRequest) {
@@ -40,7 +47,7 @@ public class ProductService {
 	public List<ProductResponse> getAllProducts() {
 		// TODO Auto-generated method stub
 		List<Product> products = productRepository.findAll();
-		return products.stream().map(this::mapToProductResponse).toList();
+		return products.stream().map(this::mapToProductResponse).collect(Collectors.toList());
 	}
 	
 	private ProductResponse mapToProductResponse(Product product) {
@@ -48,5 +55,6 @@ public class ProductService {
 				.name(product.getName())
 				.description(product.getDescription())
 				.price(product.getPrice())
-				.build();	}
+				.build();
+	}
 }
