b154e3af9965303deb93305aae3c19cfe5c6292c
Rishi <rishikesh.jswr@gmail.com>
2023-05-30 18:03:06 +0530

    RISHI-2023.5

---
+++ order-service/src/main/java/com/microservice/rishi/order_service/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.microservice.rishi.order_service.service;
+
+import com.microservice.rishi.order_service.dto.OrderLineItemsDto;
+import com.microservice.rishi.order_service.dto.OrderRequest;
+import com.microservice.rishi.order_service.model.Order;
+import com.microservice.rishi.order_service.model.OrderLineItems;
+import com.microservice.rishi.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
