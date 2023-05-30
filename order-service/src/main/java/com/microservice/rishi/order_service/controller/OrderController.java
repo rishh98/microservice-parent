950c38a762b1c29d15e4ad4e6faaf18e738e4c96
Rishi <rishikesh.jswr@gmail.com>
2023-05-30 18:41:47 +0530

    RISHI-2023.5

--- order-service/src/main/java/com/microservice/rishi/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/rishi/order_service/controller/OrderController.java
@@ -3,12 +3,14 @@ package com.microservice.rishi.order_service.controller;
 import com.microservice.rishi.order_service.dto.OrderRequest;
 import com.microservice.rishi.order_service.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
@@ -17,6 +19,19 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
+        log.info("Order Placed for {}",orderRequest.getOrderLineItemsDtoList());
         return "Order Placed Successfully";
+
+        /*
+        * Sample entry
+        * {
+    "orderLineItemsDtoList":[
+        {
+            "skuCode":"redmi_k20_pro",
+            "price":"800",
+            "quantity":1
+        }
+    ]
+}*/
     }
 }
