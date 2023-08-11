c69ebb3bca6721b2cd692c64441e500c498aa792
Rishi <rishikesh.jswr@gmail.com>
2023-08-11 09:11:50 +0530

    API_Gateway: working model

--- api-gateway/src/main/java/com/microservice/rishi/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservice/rishi/apigateway/ApiGatewayApplication.java
@@ -1,3 +1,5 @@
+package com.microservice.rishi.apigateway;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
